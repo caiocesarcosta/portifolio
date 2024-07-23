f36fd84f926cda1961ff249c693bc1310b0c1a57
caiocesarcosta <kaiocesarcosta@gmail.com>
2024-07-23 17:57:20 -0300

    update projetos

--- data.ts
+++ data.ts
@@ -48,156 +48,150 @@ export const languages: Skill[] = [
   {
     Icon: BsCircleFill,
     name: "Python",
-    level: "45",
+    level: "50",
   },
   {
     Icon: BsCircleFill,
-    name: "Java Script",
+    name: "Java",
+    level: "90",
+  },
+  {
+    Icon: BsCircleFill,
+    name: "Kotlin",
+    level: "90",
+  },
+  {
+    Icon: BsCircleFill,
+    name: "C++",
     level: "60",
   },
   {
     Icon: BsCircleFill,
-    name: "React Native",
-    level: "80",
+    name: "C#",
+    level: "60",
   },
   {
     Icon: BsCircleFill,
-    name: "React",
+    name: "Cobol",
+    level: "60",
+  },
+  {
+    Icon: BsCircleFill,
+    name: "Next.js",
+    level: "30",
+  },
+  {
+    Icon: BsCircleFill,
+    name: "MVVM",
     level: "70",
   },
   {
     Icon: BsCircleFill,
-    name: "Django",
-    level: "80",
+    name: "MVP",
+    level: "70",
   },
   {
     Icon: BsCircleFill,
-    name: "Bootstrap",
-    level: "80",
+    name: "Retrofit 2",
+    level: "70",
   },
+  {
+    Icon: BsCircleFill,
+    name: "Coroutines",
+    level: "70",
+  },
+  // {
+  //   Icon: BsCircleFill,
+  //   name: "React",
+  //   level: "70",
+  // },
+  // {
+  //   Icon: BsCircleFill,
+  //   name: "Django",
+  //   level: "80",
+  // },
+  // {
+  //   Icon: BsCircleFill,
+  //   name: "Bootstrap",
+  //   level: "80",
+  // },
 ];
 
 export const tools: Skill[] = [
   {
     Icon: BsCircleFill,
+    name: "VS Code",
+    level: "65",
+  },
+  {
+    Icon: BsCircleFill,
+    name: "Android Studio",
+    level: "90",
+  },
+  {
+    Icon: BsCircleFill,
+    name: "Linux Distro Ubunto",
+    level: "70",
+  },
+  {
+    Icon: BsCircleFill,
+    name: "MacOs",
+    level: "80",
+  },
+  {
+    Icon: BsCircleFill,
+    name: "GitHub",
+    level: "70",
+  },
+  {
+    Icon: BsCircleFill,
     name: "Figma",
-    level: "85",
+    level: "50",
   },
-  {
-    Icon: BsCircleFill,
-    name: "Photoshop",
-    level: "45",
-  },
-  {
-    Icon: BsCircleFill,
-    name: "Illustrator",
-    level: "60",
-  },
-  {
-    Icon: BsCircleFill,
-    name: "Framer",
-    level: "45",
-  },
+  
 ];
 
 export const projects: IProject[] = [
   { 
-    name: "COVID Tracker",
+    name: "SuperApp Itau",
     description:
-      "This app shows a statistical view about corona virus over the world",
-    image_path: "/images/covid.jpg",
-    deployed_url: "https://covid-19-tracker-by-sumit.web.app/",
-    github_url: "https://github.com/Dey-Sumit/covid-19-tracker",
+      "Empresa bancário, onde colaborei no app SuperApp e Cartão Branco hojé conhecido como Itaú One.  Estive em várias squads como: Cartões, Aumento de Limite, Analytics, Produtos e Empréstimos, programa de pontos Iupp",
+    image_path: "/images/itau.png",
+    deployed_url: "https://www.itau.com.br/canais-itau/aplicativo-itau",
+    // github_url: "https://github.com/Dey-Sumit/covid-19-tracker",
     category: ["react"],
-    key_techs: ["React", "Chart.js", "Material UI"],
+    key_techs: ["Java", "Kotlin", "Retrofit 2", "MVVM|MVP","Coroutines","LiveData","Observer","Koin","GitHub","Junit","Mokito", "Firebase",],
   },
   {
-    name: "Algorithm Visualizer",
-    image_path: "/images/algoVisual.png",
+    name: "Flex Automation",
+    image_path: "/images/flex-automation.png",
+    deployed_url: "https://www.flexautomation.com.br/produtos",
+    // github_url: "https://github.com/Dey-Sumit/algorithm-visualizer",
+    category: ["react"],
+    description:
+      "Empresa automação residencial. Onde contrbuí no desenvolvimento de um app de configuração e controle da valvula smart e Também no desenvolvimento do aplicativo Desktop o qual fazia o gerenciamento e configuração dos dispositivos smart (sensores, painéis touch)",
+    key_techs: ["Java", "Kotlin", "MVP", "C#", ".NET","Dagger 2", "MongoDb", "GitHub", "BlueToth","WiFi" ],
+  },
+  {
+    name: "Audit Business",
+    image_path: "/images/audit-business.jpg",
+    deployed_url: "https://www.auditbs.com.br/",
+    // github_url: "https://github.com/Dey-Sumit/algorithm-visualizer",
+    category: ["react"],
+    description:
+      "Empresa que atua no mercado de desenvolvimento de sistema para auditoria de lojas. Onde contribui com o desenvolvimento do app ControlQuest( Ferramenta de questionários online para saber as opiniões dos consulmidores que utilizam o Mall e o consumo nas loja)",
+    key_techs: ["Java","SQLITE", "MVC" ],
+  },
+  {
+    name: "Itau Baixa Plataforma - COBOL",
+    image_path: "/images/cobol.png",
     deployed_url: "https://visual-algorithm.web.app/",
-    github_url: "https://github.com/Dey-Sumit/algorithm-visualizer",
+    // github_url: "https://github.com/Dey-Sumit/algorithm-visualizer",
     category: ["react"],
     description:
-      "An web app which shows how an algorithm (path finding or sorting) works with cool animation",
-    key_techs: ["React", "firebase", "Framer Motion"],
+      "Empresa bancária. onde colaboreis na codificação em COBOl ",
+    key_techs: ["COBOL","DB2"],
   },
 
-  {
-    name: "Dev Talks",
-    image_path: "/images/dev.jpg",
-    deployed_url: "https://dev-talks.herokuapp.com/",
-    github_url: "https://github.com/Dey-Sumit/Dev-talks",
-    category: ["node", "mongo", "react"],
-    description:
-      "Social Media app for developers who can share project,create posts,etc...",
-    key_techs: [
-      "React",
-      "Redux",
-      "Node",
-      "Express",
-      "Mongo",
-      "REST API",
-      "Bootstrap",
-    ],
-  },
-
-  {
-    name: "Realtime Chat App",
-    image_path: "/images/chatapp.jpg",
-    deployed_url: "https://sumit-chat.netlify.app/",
-    github_url: "https://github.com/Dey-Sumit/chat-app-socket.io-react-node",
-    category: ["node", "react"],
-    description:
-      "Basic Realtime Chat App where one can create a room can talk to each other",
-    key_techs: ["React", "Node", "Express", "Socket", "Bootstrap"],
-  },
-
-  {
-    name: "Tweeter Clone",
-    image_path: "/images/tweetme.jpg",
-    deployed_url: "http://sumaxtweetme.pythonanywhere.com/",
-    github_url: "https://github.com/Dey-Sumit/tweetme",
-    category: ["django", "react"],
-    description:
-      "First Django Project :) | Typical Social Media App where one can post,like ,comment etc",
-    key_techs: ["React", "Django", "Django REST API"],
-  },
-
-  {
-    name: "Color Classification using tf.js",
-    image_path: "/images/color.jpg",
-    deployed_url: "!#",
-    github_url: "https://github.com/Dey-Sumit/color-classification",
-    category: ["express"],
-    description:
-      "Tried ML with JS :) | this app classifies a color using CNN algorithm in browser",
-    key_techs: ["Express", "TensorFlow.js", "Vanilla js"],
-  },
-  {
-    name: "YouTube using YouTube ",
-    image_path: "/images/youtubeClone.png",
-    deployed_url: "https://not-utube.web.app/",
-    github_url: "https://github.com/Dey-Sumit/youtube-clone-tutorial-up",
-    category: ["express"],
-    description:
-      'Full(almost) Functional YouTube replica where one can login with his/her youtube account to enjoy "not-YouTube".User can like a video,comment on a video & Much More ',
-    key_techs: [
-      "React",
-      "Redux",
-      "Firebase Auth",
-      "YouTube API",
-      "Sass",
-      "Bootstrap",
-    ],
-  },
-  {
-    name: "Football App",
-    image_path: "/images/football.png",
-    deployed_url: "https://o-my-goal.web.app/",
-    github_url: "https://github.com/Dey-Sumit/football-app",
-    category: ["react"],
-    description:
-      "o my goal replica where an user can keep an eye on his favorite club.This app will keep providing \n all the statistics of that club.all the fans can also chat ",
-    key_techs: ["React", "Redux", "Firebase Auth", "API", "Sass", "Bootstrap"],
-  },
+  
 ];
