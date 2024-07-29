e7fcd7e231910d592a1aa7655b53438b3b388eb8
caiocesarcosta <kaiocesarcosta@gmail.com>
2024-07-29 20:49:42 -0300

    update next

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 import "../styles/globals.css";
-import { ThemeProvider } from "next-themes";
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { ThemeProvider } from "next-themes";
 
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider >
+    <ThemeProvider attribute="class">
       <div className="grid grid-cols-12 gap-6 px-5 my-14 lg:mb-0 md:mb-16 sm:px-20 md:px-32 lg:px-36 xl:px-48 ">
         {/* // do this div style later (after putting the content) */}
         <div className="h-full col-span-12 p-4 text-base text-center bg-white dark:bg-dark-500 lg:col-span-3 rounded-2xl shadow-custom-light dark:shadow-custom-dark ">
