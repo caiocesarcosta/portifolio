a5fe6b3b5ac97672ca863b0daeb9adc131159504
caiocesarcosta <kaiocesarcosta@gmail.com>
2024-08-05 19:32:27 -0300

    fixed error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,8 @@ import Sidebar from "../components/Sidebar";
 import "../styles/globals.css";
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from "@vercel/speed-insights/next"       
-
-
-
 import { ThemeProvider } from "next-themes";
-// import { ThemeProvider } from 'styled-components';
+
 
 
 function MyApp({ Component, pageProps }) {
